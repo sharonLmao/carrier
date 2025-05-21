e4fb5190d3972151161e4a8563814212f8830ef6
Sharon Shamay <84649681+sharonLmao@users.noreply.github.com>
2025-05-22 02:21:02 +0300

    Init
    Guards are not there anymore yay

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'sharon9949'
+description 'The map for the carrier heist, fuck ye'
+version '1.0.2'
+
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/*',
+}
